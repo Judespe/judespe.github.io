e3640e56e93ae415087c1a5ed0a17576433a213a
judespe <julien.dessainjean@gmail.com>
2016-11-07 16:43:00 +0100

    Avancement Projet : session storage
    Implémentation des variables sessionStorage OK

--- js/reservationVelos.js
+++ js/reservationVelos.js
@@ -1,4 +1,129 @@
-//$(function() {
+//////////////////////////////////////////////////////
+/////////// Gestion Google Map + Formulaire //////////
+//////////////////////////////////////////////////////
+
+function ajaxGet(url, callback) {
+	var req = new XMLHttpRequest();
+	req.open('GET', url);
+	req.addEventListener('load', function() {
+		if (req.status >= 200 && req.status < 400) {
+			// Appelle la fonction callback en lui passant la réponse en paramètre
+			callback(req.responseText);
+		} else {
+			console.error(req.status + ' ' + req.statusText + ' ' + url);
+		}
+	});
+	req.addEventListener('error', function() {
+		console.error('Erreur réseau avec l\'URL ' + url);
+	});
+	req.send(null);
+}
+
+var map;
+
+function initMap() {
+
+	// Création de la Google Map
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 48.856614, lng: 2.3522219000000177},
+    zoom: 12
+  });
+
+  // Appel à l'API Ville de Paris
+	ajaxGet('http://opendata.paris.fr/api/records/1.0/search/?dataset=stations-velib-disponibilites-en-temps-reel&rows=1224&facet=banking&facet=bonus&facet=status&facet=contract_name', function(reponse) {
+
+		var donnees = JSON.parse(reponse);
+		var	stations = donnees.records;
+		var markers = [];
+
+		// Récupération des coordonnées de chaque borne et création des marqueurs sur la Google Map 
+		stations.forEach(function(station) {
+			var latitudeStation = station.fields.position[0],
+					longitudeStation = station.fields.position[1];
+			var marker = new google.maps.Marker({
+				map: map,
+				position: {lat: latitudeStation, lng: longitudeStation}
+			});
+			markers.push(marker);
+
+			var nomStation = station.fields.name,
+					adresseStation = station.fields.address,
+					velosTotalStation = station.fields.bike_stands,
+					velosLibresStation = station.fields.available_bikes,
+					emplacementsLibresStation = station.fields.available_bike_stands,
+					statutStation = station.fields.status,
+					statutStationFr = '';
+
+					if (statutStation === "OPEN") {
+						statutStationFr = 'Ouverte';
+					} else if (statutStation === "CLOSED") {
+						statutStationFr = 'Fermée';
+					} else {
+						statutStationFr = 'Non Disponible';
+					}
+			// Affichage formulaire quand clic sur un marker
+			marker.addListener('click', function() {
+				$('#details').html('');
+				$('#signature').fadeOut();
+				$('#erreur').fadeOut();
+				var titreEtiquette = document.createElement('h4');
+				titreEtiquette.innerHTML = 'Station n°<span id="nomStation">' + nomStation + '</span>';
+				
+				var infosAdresseEtiquette = document.createElement('p');
+				infosAdresseEtiquette.innerHTML = 'Adresse : ' + adresseStation;
+
+				var statutStationEtiquette = document.createElement('p');
+				statutStationEtiquette.id = 'statut';
+
+				if (statutStation === "OPEN") {
+					statutStationEtiquette.innerHTML = 'Station ' + statutStationFr;
+					statutStationEtiquette.className = 'ouverte';
+					$('#bouton_reserver').removeAttr('disabled'); 
+				} else if (statutStation === "CLOSED") {
+					statutStationEtiquette.innerHTML = 'Station ' + statutStationFr;
+					statutStationEtiquette.className = 'fermee'; 
+					$('#bouton_reserver').attr('disabled', 'disabled');
+				} else {
+					statutStationEtiquette.innerHTML = 'Station ' + statutStationFr + '</span>';
+					statutStationEtiquette.className = 'indefini';
+					$('#bouton_reserver').attr('disabled', 'disabled');
+				}
+
+				var infosVelos = document.createElement('p');
+				infosVelos.innerHTML = 'Capacité de la station : ' + velosTotalStation + '<br>Nombre de vélos libres : ' + velosLibresStation + '<br>Nombre d\'emplacements libres : ' + emplacementsLibresStation;
+
+				$('#details').append(titreEtiquette, infosAdresseEtiquette, statutStationEtiquette, infosVelos);
+
+				$('#bloc_infos_station').fadeIn();
+			}); // Fin de l'événement "clic" sur les markers
+
+		}); // Fin de la boucle "stations.forEach"
+
+		// Ouverture du canvas au clic sur le bouton Réserver (si activé)
+		$('#bouton_reserver:enabled').click(function() {
+			$('#signature').fadeIn();
+
+			var offsetSignature = $('#signature').offset().top;
+			$('html, body').animate({scrollTop: offsetSignature}, 1000);
+			return false;
+		});
+
+		// Fermeture formulaire de réservation si clic en dehors du bloc "carte-forulaire"
+		$(document.body).click(function(e) {
+			var carteDetails = $('#carte_details');
+			if (!$(e.target).is(carteDetails) && !$.contains(carteDetails[0], e.target)) {
+				$('#bloc_infos_station').fadeOut();
+				$('#signature').fadeOut();
+			}
+		});
+
+		// Utilisation de l'objet markerClusterer (regroupement automatique des marqueurs proches) 
+		var markerCluster = new MarkerClusterer(map, markers,{imagePath: 'images/m'});
+	}); // Fin appel API Ville de Paris
+
+} // Fin de la fontion initMap
+
+(function($) {
 
 	////////////////////////////////////////
 	/////////// Gestion diaporama //////////
@@ -12,143 +137,67 @@
 		$('#bloc_photos div:last-child').remove().prependTo('#bloc_photos');
 	});
 
-	//////////////////////////////////////////////////////
-	/////////// Gestion Google Map + Formulaire //////////
-	//////////////////////////////////////////////////////
+	/////////////////////////////////////////
+	/////////// Gestion canvas //////////////
+	/////////////////////////////////////////
 
-	function ajaxGet(url, callback) {
-		var req = new XMLHttpRequest();
-		req.open('GET', url);
-		req.addEventListener('load', function() {
-			if (req.status >= 200 && req.status < 400) {
-				// Appelle la fonction callback en lui passant la réponse en paramètre
-				callback(req.responseText);
-			} else {
-				console.error(req.status + ' ' + req.statusText + ' ' + url);
+	// Gestion du compte à rebours
+	var decompte;
+
+	function diminuerCompteur() {
+		// Déclaration des variables
+		var minutesElt = document.getElementById('minutes'),
+				secondesElt = document.getElementById('secondes'),
+				minutes = Number(minutesElt.textContent),
+				secondes = Number(secondesElt.textContent),
+				timeBefore;
+
+		// Décrémentation du temps et sauvegarde du temps en sessionStorage à chaque boucle
+		if (secondes > 0) {
+			timeBefore = new Date().getTime();
+			secondesElt.textContent = secondes - 1;
+			sessionStorage.setItem('secondesBefore', secondesElt.textContent),
+			sessionStorage.setItem('timeBefore', timeBefore);
+		} else {
+			if (minutes > 0) {
+				minutesElt.textContent = minutes - 1;
+				secondesElt.textContent = '59';
+				timeBefore = new Date().getTime();
+				sessionStorage.setItem('secondesBefore', secondesElt.textContent);
+				sessionStorage.setItem('minutesBefore', minutesElt.textContent);
+				sessionStorage.setItem('timeBefore', timeBefore);
+			} else if (minutes <= 0) {
+				$('#message_reservation').hide().html('<span class="alert">La durée de 20 minutes est écoulée, votre réservation a été annulée</span>').fadeIn();
+				clearInterval(decompte);
+				sessionStorage.setItem('station', '');
 			}
-		});
-		req.addEventListener('error', function() {
-			console.error('Erreur réseau avec l\'URL ' + url);
-		});
-		req.send(null);
+		}
 	}
-
-	var map;
-
-	function initMap() {
-
-		// Création de la Google Map
-	  map = new google.maps.Map(document.getElementById('map'), {
-	    center: {lat: 48.856614, lng: 2.3522219000000177},
-	    zoom: 12
-	  });
-
-	  // Appel à l'API Ville de Paris
-		ajaxGet('http://opendata.paris.fr/api/records/1.0/search/?dataset=stations-velib-disponibilites-en-temps-reel&rows=1224&facet=banking&facet=bonus&facet=status&facet=contract_name', function(reponse) {
-
-			var donnees = JSON.parse(reponse);
-			var	stations = donnees.records;
-			var markers = [];
-
-			// Récupération des coordonnées de chaque borne et création des marqueurs sur la Google Map 
-			stations.forEach(function(station) {
-				var latitudeStation = station.fields.position[0],
-						longitudeStation = station.fields.position[1];
-				var marker = new google.maps.Marker({
-					map: map,
-					position: {lat: latitudeStation, lng: longitudeStation}
-				});
-				markers.push(marker);
-
-				var nomStation = station.fields.name,
-						adresseStation = station.fields.address,
-						velosTotalStation = station.fields.bike_stands,
-						velosLibresStation = station.fields.available_bikes,
-						emplacementsLibresStation = station.fields.available_bike_stands,
-						statutStation = station.fields.status,
-						statutStationFr = '';
-
-						if (statutStation === "OPEN") {
-							statutStationFr = 'Ouverte';
-						} else if (statutStation === "CLOSED") {
-							statutStationFr = 'Fermée';
-						} else {
-							statutStationFr = 'Non Disponible';
-						}
-				// Affichage formulaire quand clic sur un marker
-				marker.addListener('click', function() {
-					$('#details').html('');
-					$('#signature').fadeOut();
-					$('#erreur').fadeOut();
-					var titreEtiquette = document.createElement('h4');
-					titreEtiquette.innerHTML = 'Station n°<span id="nomStation">' + nomStation + '</span>';
-					
-					var infosAdresseEtiquette = document.createElement('p');
-					infosAdresseEtiquette.innerHTML = 'Adresse : ' + adresseStation;
-
-					var statutStationEtiquette = document.createElement('p');
-					statutStationEtiquette.id = 'statut';
-
-					if (statutStation === "OPEN") {
-						statutStationEtiquette.innerHTML = 'Station ' + statutStationFr;
-						statutStationEtiquette.className = 'ouverte';
-						$('#bouton_reserver').removeAttr('disabled'); 
-					} else if (statutStation === "CLOSED") {
-						statutStationEtiquette.innerHTML = 'Station ' + statutStationFr;
-						statutStationEtiquette.className = 'fermee'; 
-						$('#bouton_reserver').attr('disabled', 'disabled');
-					} else {
-						statutStationEtiquette.innerHTML = 'Station ' + statutStationFr + '</span>';
-						statutStationEtiquette.className = 'indefini';
-						$('#bouton_reserver').attr('disabled', 'disabled');
-					}
-
-					var infosVelos = document.createElement('p');
-					infosVelos.innerHTML = 'Capacité de la station : ' + velosTotalStation + '<br>Nombre de vélos libres : ' + velosLibresStation + '<br>Nombre d\'emplacements libres : ' + emplacementsLibresStation;
-
-					$('#details').append(titreEtiquette, infosAdresseEtiquette, statutStationEtiquette, infosVelos);
-
-					$('#bloc_infos_station').fadeIn();
-				}); // Fin de l'événement "clic" sur les markers
-
-			}); // Fin de la boucle "stations.forEach"
-
-			// Ouverture du canvas au clic sur le bouton Réserver (si activé)
-			$('#bouton_reserver:enabled').click(function() {
-				$('#signature').fadeIn();
-
-				var offsetSignature = $('#signature').offset().top;
-				$('html, body').animate({scrollTop: offsetSignature}, 1000);
-				return false;
-			});
-
-			// Fermeture formulaire de réservation si clic en dehors du bloc "carte-forulaire"
-			$(document.body).click(function(e) {
-				var carteDetails = $('#carte_details');
-				if (!$(e.target).is(carteDetails) && !$.contains(carteDetails[0], e.target)) {
-					$('#bloc_infos_station').fadeOut();
-					$('#signature').fadeOut();
-				}
-			});
-
-			// Utilisation de l'objet markerClusterer (regroupement automatique des marqueurs proches) 
-			var markerCluster = new MarkerClusterer(map, markers,{imagePath: 'images/m'});
-		}); // Fin appel API Ville de Paris
-
-	} // Fin de la fontion initMap
-
-/////////////////////////////////////////
-/////////// Gestion canvas //////////////
-/////////////////////////////////////////
-
-$(function() {
 
 	// Chargement des réservations en cours avec sessionStorage
 	function chargerReservation() {
-	  if (sessionStorage.getItem('reservation')) {
-	  	document.getElementById('storage_data').innerHTML = sessionStorage.getItem('reservation');
+	  if (sessionStorage.getItem('station')) {
+
+	  	// Calcul du temps de réservation restant
+	  	var timestampAfter = new Date().getTime();
+	  	var minutesBefore = sessionStorage.getItem('minutesBefore'),
+	  			secondesBefore = sessionStorage.getItem('secondesBefore'),
+	  			timestampBefore = sessionStorage.getItem('timeBefore'),
+	  			timeBefore,	timeLeft, timeReload, timeReloadRound;
+	  			
+	  	timeReload = timestampAfter - timestampBefore;
+	  	timeReloadRound = Math.round(timeReload / 1000);
+	  	timeBefore = (minutesBefore * 60) + secondesBefore;
+	  	timeLeft = timeBefore - timeReloadRound;
+	  	secondesLeft = (timeLeft % 60);
+	  	minutesLeft = Math.floor((timeLeft - secondesLeft) / 6000);
+
+	  	// Affectation du temps restant dans le champ #storage_data (si temps inférieur à 20 minutes)
+	  	$('#nom_station').html(sessionStorage.getItem('station'));
+	  	$('#minutes').html(minutesLeft);
+	  	$('#secondes').html(secondesLeft);
 	  	$('#resume').fadeIn();
-	  	$('#message_reservation').hide();
+	  	decompte = setInterval(diminuerCompteur, 1000);
 	  }
 	}
 	chargerReservation();
@@ -242,26 +291,6 @@ $(function() {
 			blankCanvas = document.getElementById('blank'),
 			nomStationRecup = $('#nomStation').text();
 
-	var decompte;
-	function diminuerCompteur() {
-		var minutesElt = document.getElementById('minutes'),
-				secondesElt = document.getElementById('secondes'),
-				minutes = Number(minutesElt.textContent),
-				secondes = Number(secondesElt.textContent);
-
-		if (secondes > 0) {
-			secondesElt.textContent = secondes - 1;
-		} else {
-			if (minutes > 0) {
-				minutesElt.textContent = minutes - 1;
-				secondesElt.textContent = '59';
-			} else if (minutes === 0) {
-				$('#message_reservation').hide().html('<span class="alert">La durée de 20 minutes est écoulée, votre réservation a été annulée</span>').fadeIn();
-				clearInterval(decompte);
-			}
-		}
-	}
-
 	$('#bouton_valider').click(function() {
 		
 		// Affichage message d'erreur si pas de signature dans le canvas
@@ -271,10 +300,12 @@ $(function() {
 		// Validation de la réservation, affichage de la réservation dans la section "résumé" et lancement du timer de 20 minutes
 		else {
 			clearInterval(decompte);
-			var nomStationRecup = $('#nomStation').text();
+			var nomStationRecup = $('#nomStation').text(),
+					init;
 
 			$('#storage_data').hide();
 			$('#message_reservation').hide();
+			$('#erreur').hide();
 			$('#signature').fadeOut();
 			clearCanvas();
 			$('#nom_station').text(nomStationRecup);
@@ -284,8 +315,10 @@ $(function() {
 			$('#resume').fadeIn();
 			decompte = setInterval(diminuerCompteur, 1000);
 
-			var donneesReservation = document.getElementById('message_reservation').textContent;
+			var donneesReservation = document.getElementById('message_reservation').innerHTML;
 			sessionStorage.setItem('reservation', donneesReservation);
+			var stationReservation = $('#nom_station').text();
+			sessionStorage.setItem('station', stationReservation);
 
 			var offsetResume = $('#resume').offset().top;
 			$('html, body').animate({scrollTop: offsetResume}, 1000);
@@ -298,4 +331,4 @@ checkWidthWindow();
 
 //////////////// Fin Gestion canvas //////////////////
 
-});
+})(jQuery);
